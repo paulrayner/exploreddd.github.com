2c0fd1785686ff80bd6b9b4693f93edb9a1c6f5a
Christine de Carteret <Christine@Christines-MacBook-Pro.local>
2017-01-01 19:45:52 -0700

    Adding slider functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 
+    $(function(){
+      SyntaxHighlighter.all();
+    });
+    $(window).load(function(){
+      $('.flexslider').flexslider({
+        animation: "fade",
+        animationLoop: "true",
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+    });
