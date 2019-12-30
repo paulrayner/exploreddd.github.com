801da08889aebe8d8f5b5eab6a5ad50dd065835b
Zachariah Michaelonious Aquinas <zach.johnston@gmail.com>
2019-12-30 08:02:25 -0700

    hero video full window height fluidity has been instantiated for test

--- src/js/main.js
+++ src/js/main.js
@@ -97,4 +97,21 @@ $('.showSingle').click(function(){
   });
 });
 
+$(document).ready(function() {
+  $(window).resize(function() {
+      var bodyheight = $(this).height();
+      var navheight = $('.navbar').outerHeight(true);
+      var homepagetopbar = $('.homepage--top-bar').outerHeight(true);
+      $("#sidebar").height(bodyheight);
+      console.log('height of window is ' + bodyheight);
+      console.log('height of nav div is ' + navheight);
+      console.log('height of homepage top bar div is ' + homepagetopbar);
 
+      var videoheight = bodyheight - navheight - homepagetopbar;
+
+      $(".homepage--hero-video-container").css("margin-top",homepagetopbar);
+
+      $(".homepage--hero-video-container").css("height",videoheight);
+
+  }).resize();
+});
