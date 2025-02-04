6d13d6e26fd7377dca2537f05061d92cb5428637
Jessica Kerr <jessitron@gmail.com>
2025-02-03 21:33:59 -0600

    Updated dependencies, only used ones

--- index.js
+++ index.js
@@ -1,71 +1,81 @@
-var Metalsmith        = require('metalsmith'),
-    markdown          = require('metalsmith-markdown'),
-    layouts           = require('metalsmith-layouts'),
-    sass              = require('metalsmith-sass'),
-    discoverPartials  = require('metalsmith-discover-partials'),
-    watch             = require('metalsmith-watch');
+var Metalsmith = require("metalsmith"),
+  markdown = require("@metalsmith/markdown"),
+  layouts = require("@metalsmith/layouts"),
+  sass = require("@metalsmith/sass"),
+  discoverPartials = require("metalsmith-discover-partials"),
+  watch = require("metalsmith-watch");
 
 //var googleAnalytics = require('metalsmith-google-analytics')
 
 var dir = {
-  base:   __dirname + '/',
-  lib:    __dirname + '/lib/',
-  source: './src/',
-  dest:   './docs/'
+  base: __dirname + "/",
+  lib: __dirname + "/lib/",
+  source: "./src/",
+  dest: "./docs/",
 };
 
 Metalsmith(__dirname)
   .metadata({
     title: "Explore DDD Conference - 2025",
-    description: "Explore DDD Conference - 2025: Domain-Driven Design Conference in Denver, Colorado, USA for Software Leaders. Practical, cutting-edge, talks &amp; workshops.",
+    description:
+      "Explore DDD Conference - 2025: Domain-Driven Design Conference in Denver, Colorado, USA for Software Leaders. Practical, cutting-edge, talks &amp; workshops.",
     generator: "Metalsmith",
-    url: "http://www.exploreddd.com"
+    url: "http://www.exploreddd.com",
   })
-  .source('./src')
-  .destination('./docs')
+  .source("./src")
+  .destination("./docs")
   .clean(false)
   .use(markdown())
   // .use(permalinks())
-  .use(discoverPartials({
-    directory: 'partials',
-    pattern: /\.hbs$/
-  }))
-  .use(layouts({
-    engine: 'handlebars'
-  }))
-  .use(sass({
-    outputDir: 'css/',
-    outputStyle: 'compressed'
-  }))
-//  .use(googleAnalytics('UA-37443128-6'))
+  .use(
+    discoverPartials({
+      directory: "partials",
+      pattern: /\.hbs$/,
+    })
+  )
+  .use(
+    layouts({
+      engine: "handlebars",
+    })
+  )
+  .use(
+    sass({
+      outputDir: "css/",
+      outputStyle: "compressed",
+    })
+  )
+  //  .use(googleAnalytics('UA-37443128-6'))
   // .use(metadata({
   //   ericEvans: 'data/eric-evans.json'
   // }))
-  .use(watch({
-     paths: {
-       "${source}/**/*": true,
-       "partials/**/*": "**/*.md",
-     },
-     livereload: true,
-   }))
-  .build(function(err, files) {
-    if (err) { throw err; }
+  .use(
+    watch({
+      paths: {
+        "${source}/**/*": true,
+        "partials/**/*": "**/*.md",
+      },
+      livereload: true,
+    })
+  )
+  .build(function (err, files) {
+    if (err) {
+      throw err;
+    }
   });
-
 
 // Debug function
 function debug(logToConsole) {
-  return function(files, metalsmith, done) {
+  return function (files, metalsmith, done) {
     if (logToConsole) {
-      console.log('\nMETADATA:');
+      console.log("\nMETADATA:");
       console.log(metalsmith.metadata());
 
       for (var f in files) {
-        console.log('\nFILE:');
+        console.log("\nFILE:");
         console.log(files[f]);
       }
     }
 
     done();
   };
-};
+}
